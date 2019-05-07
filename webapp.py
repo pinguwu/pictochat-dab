7f640e4c4a6a7e2c95929a7b4132a130166c14ea
pinguwu <premixedcloud977@gmail.com>
2019-05-07 16:13:45 -0700

    'l'

---
+++ webapp.py
@@ -0,0 +1,19 @@
+#adapted from https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example
+
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode=None)
+thread = None
+thread_lock = Lock() #we'll use this lock to prevent multiple clients from modifying thread at the same time
+
+
+@app.route('/')
+def index():
+    return render_template('home.html', async_mode=socketio.async_mode)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
