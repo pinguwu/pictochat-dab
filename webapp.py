bd6aad53355cbdefd85dd73f5f20ecb304547f6c
O-bjective <42652904+O-bjective@users.noreply.github.com>
2019-05-10 10:59:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,20 @@
-#adapted from https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example
-
+from flask_oauthlib.client import OAuth
+from flask import render_template
+from bson.objectid import ObjectId
 from threading import Lock
-from flask import Flask, render_template, session, request
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
-app = Flask(__name__)
+import pymongo
+import sys
+import dns
+import pprint
+import os
+#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+app = Flask(__name__).
+app.debug = True
 socketio = SocketIO(app, async_mode=None)
 thread = None
 thread_lock = Lock() 
