b9b265589694a2ab2ef194e081da42f889059aa7
pinguwu <premixedcloud977@gmail.com>
2019-05-15 11:35:46 -0700

    'paris'

--- webapp.py
+++ webapp.py
@@ -17,24 +17,46 @@ app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app, async_mode=None)
 thread = None
-thread_lock = Lock() 
+thread_lock = Lock()
+
+chat1_data = {}
+chat2_data = {}
+chat3_data = {}
+chat4_data = {}
 
 
 @app.route('/')
 def index():
-    return render_template('home.html', async_mode=socketio.async_mode)
+    return render_template('home.html')
 
 @app.route('/Chat1')
 def renderchat1():
-    
+    return render_template('chat1.html', async_mode = socketio.async_mode)
+
 @app.route('/Chat2')
 def renderchat2():
-    
+    return render_template('chat2.html', async_mode = socketio.async_mode)
+
 @app.route('/Chat3')
 def renderchat3():
-    
+    return render_template('chat3.html', async_mode = socketio.async_mode)
+
 @app.route('/Chat4')
 def renderchat4():
-    
+    return render_template('chat4.html', async_mode = socketio.async_mode)
+
+def background_thread_1():
+    pass
+
+def background_thread_2():
+    pass
+
+def background_thread_3():
+    pass
+
+def background_thread_4():
+    pass
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
